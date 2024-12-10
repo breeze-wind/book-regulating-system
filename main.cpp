df7772cbea9b227a89bdb3d1ce007b767256d21f
sfx233 <sfx233@nuaa.edu.cn>
2024-12-10 22:54:49 +0800

    存档(收银模块写完了）

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <cstring>
 #include <fstream>
+#include <iomanip>
 using namespace std;
-class BookData
+class BookData//定义书籍信息类
 {
 public:
     BookData()
     {
-        isexist=true;
+        isexist=false;//初始化认为书不存在
     };
     ~BookData()=default;
     void setTitle(string& title)
@@ -39,6 +40,19 @@ public:
     {
     	return this->isbn;
 	}
+	string bookauthor(void)
+    {
+	    return this->bookauthor;
+    }
+	int qtyOnHand(void)
+    {
+	    return this->qtyOnHand;
+    }
+	double retail(void)
+    {
+	    return this -> retail;
+    }
+
 	
 private:
     string bookTitle;
@@ -47,14 +61,24 @@ private:
     double retail;
     string bookauthor;
 
-
-
 };
+BookData operator -(BookData& book,int a)
+{
+	book.setQty(book.qtyOnHand-a);
+	if (book.qtyOnHand<=0)
+	{
+		book.isexist=false;
+		book.bookTitle[0]='\0';
+	}
+	return book;
+}
+BookData books[200];//书库容器
 void  lookUpBook(string);
 void Bookinfo(BookData&);
+void cash_managing(void);
 bool isempty(int i)
 {
-
+return books[i].isexist;
 }
 void blockchoose()
 {
@@ -84,42 +108,72 @@ void blockchoose()
 	 		break;
 	 }
 	 
-}
+}//
 void cash_managing(void)
 {
-	string a;
+	string a;//isbn
+	int b=0;//数量
 	cout<<"\t前台销售模块\t"<<endl;
-	cout<<"输入想买的书ISBN"<<engl;
-	cin<<a; 
-	for()
-	cout<<"日期：__DATE__"<<endl;
+	cout<<"输入想买的书ISBN"<<endl;
+	cin<<a;
+	int i=0;
+	while(isempty(i))
+	{
+		if (books[i].isbn==a)
+			break;
+		i++;
+	}
+	if (!books[i].isexist)
+	{
+		cout<<"卖光了T_T  看看别的吧"<<endl;
+		 cash_managing();
+		return;
+	}
+	cout<<"日期："<<__DATE__<<endl;
+	cout<<"库存数量\tISBN号\t书名\t单价"<<endl;
+	cout<<books[i].qtyOnHand<<"\t"<<books[i].isbn<<"\t"<<books[i].bookTitle<<"\t"<<books[i].retail<<"元\t"<<endl;
 	cout<<"数量\tISBN号\t书名\t单价\t金额"<<endl;
-	cout<< 
+	cin>>b;
+	if (b>=books[i].qtyOnHand)
+	{
+		cout<<"库存不足T_T看看别的？"<<endl;
+		cash_managing();
+		return;
+	}
+	double b1 = b*(books[i].retail);
+	cout<<"\t"<<books[i].isbn<<"\t"<<books[i].bookTitle<<"\t"<<books[i].retail<<"元\t"<<b1<<"元\t"<<endl;
+	cout<<"----------------------------------------------"<<endl;
+	cout<<"销售合计:RMB "<<b1<<endl;
+	cout<<"零售税"<<fixed<<setprecision(2)<<b1*0.06<<endl;
+	cout<<"应付金额"<<fixed<<setprecision(2)<<b1*1.06<<endl;
+	cout<<"\n\n谢谢光临！";
+	books[i]-b;
 }
 int main()
 {
-    BookData books[200];
-    string title;
+ BookData books[200];//书库容器
+	string title;
     string author;
     int qtyOnHand;
     double retail;
     string isbn;
-    ifstream bookfile("bookfile");
+    ifstream basic_ifstream("bookfile");
     int i=0;
-    if (ifstream bookfile()==false)
+    if (basic_ifstream()==false)
     {
         cout<<"读取书库失败"<<endl;
         return 0;
     }
-    while (bookfile>>title>>isbn>>author>>qtyOnHand>>retail)
+    while (basic_ifstream>>title>>isbn>>author>>qtyOnHand>>retail)
     {
         books[i].setTitle(title);
         books[i].setAuthor(author);
         books[i].setQty(qtyOnHand);
         books[i].setISBN(isbn);
         books[i].setRetail(retail);
+    	books[i].isexist=true;
     }//读取书库
-    bookfile.close();
-    void blockchoose();
+    basic_ifstream.close();//关闭文件输入流
+    void blockchoose();//进入模块选择
     
 }
