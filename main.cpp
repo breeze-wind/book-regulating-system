fc290203214a415c596b0fb7c9ca9833ea7c24d8
sfx233 <sfx233@nuaa.edu.cn>
2024-12-11 17:02:02 +0800

    存档(收银模块写完了）2

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #include <iomanip>
 using namespace std;
-class BookData//定义书籍信息类
+class BookData
 {
 public:
     BookData()
@@ -13,15 +13,15 @@ public:
     ~BookData()=default;
     void setTitle(string& title)
     {
-        this->bookTitle =title;
+        this->booktitle =title;
     }
     void setAuthor(string& author)
     {
-       this->bookauthor = author;
+       this->bookAuthor = author;
     }
     void setQty(int qty)
     {
-        this->qtyOnHand = qty;
+        this->QtyOnHand = qty;
     }
     void setISBN(string& isbn)
     {
@@ -29,56 +29,63 @@ public:
     }
     void setRetail(double retail)
     {
-        this->retail = retail;
+        this->Retail = retail;
     }
     bool isexist;
     string bookTitle(void)
     {
-    	return this->bookTitle;
+    	return this->booktitle;
 	}
-	   string isbn(void)
+	   string ISBN(void)
     {
     	return this->isbn;
 	}
 	string bookauthor(void)
     {
-	    return this->bookauthor;
+	    return this->bookAuthor;
     }
 	int qtyOnHand(void)
     {
-	    return this->qtyOnHand;
+	    return this->QtyOnHand;
     }
 	double retail(void)
     {
-	    return this -> retail;
+	    return this -> Retail;
     }
 
 	
 private:
-    string bookTitle;
+    string booktitle;
     string isbn;
-    int qtyOnHand;
-    double retail;
-    string bookauthor;
+    int QtyOnHand;
+    double Retail;
+    string bookAuthor;
 
-};
+};//定义书籍信息类
 BookData operator -(BookData& book,int a)
 {
-	book.setQty(book.qtyOnHand-a);
-	if (book.qtyOnHand<=0)
+	book.setQty(book.qtyOnHand()-a);
+	if (book.qtyOnHand()<=0)
 	{
 		book.isexist=false;
-		book.bookTitle[0]='\0';
+		//book.bookTitle[0]='\0';
 	}
 	return book;
 }
+
+void bookfile_managing(void);
 BookData books[200];//书库容器
 void  lookUpBook(string);
 void Bookinfo(BookData&);
 void cash_managing(void);
+void sheeting(void);
+void system_exit(void);
 bool isempty(int i)
 {
-return books[i].isexist;
+ if ((books[i].bookTitle())[0]=='\0')
+	return false;
+	else
+		return true;
 }
 void blockchoose()
 {
@@ -101,10 +108,10 @@ void blockchoose()
 	 		bookfile_managing();
 	 		break;
 	 	case 3:
-	 		sheeting();
+	 		//sheeting();
 	 		break;
 	 	case 4:
-	 		system_exit();
+	 		//system_exit();
 	 		break;
 	 }
 	 
@@ -115,11 +122,12 @@ void cash_managing(void)
 	int b=0;//数量
 	cout<<"\t前台销售模块\t"<<endl;
 	cout<<"输入想买的书ISBN"<<endl;
-	cin<<a;
-	int i=0;
+	cin>>a;
+	int i;
+	i = 0;
 	while(isempty(i))
 	{
-		if (books[i].isbn==a)
+		if (books[i].ISBN()==a)
 			break;
 		i++;
 	}
@@ -131,17 +139,17 @@ void cash_managing(void)
 	}
 	cout<<"日期："<<__DATE__<<endl;
 	cout<<"库存数量\tISBN号\t书名\t单价"<<endl;
-	cout<<books[i].qtyOnHand<<"\t"<<books[i].isbn<<"\t"<<books[i].bookTitle<<"\t"<<books[i].retail<<"元\t"<<endl;
+	cout<<books[i].qtyOnHand()<<"\t"<<books[i].ISBN()<<"\t"<<books[i].bookTitle()<<"\t"<<books[i].retail()<<"元\t"<<endl;
 	cout<<"数量\tISBN号\t书名\t单价\t金额"<<endl;
 	cin>>b;
-	if (b>=books[i].qtyOnHand)
+	if (b>=books[i].qtyOnHand())
 	{
 		cout<<"库存不足T_T看看别的？"<<endl;
 		cash_managing();
 		return;
 	}
-	double b1 = b*(books[i].retail);
-	cout<<"\t"<<books[i].isbn<<"\t"<<books[i].bookTitle<<"\t"<<books[i].retail<<"元\t"<<b1<<"元\t"<<endl;
+	double b1 = b*(books[i].retail());
+	cout<<"\t"<<books[i].ISBN()<<"\t"<<books[i].bookTitle()<<"\t"<<books[i].retail()<<"元\t"<<b1<<"元\t"<<endl;
 	cout<<"----------------------------------------------"<<endl;
 	cout<<"销售合计:RMB "<<b1<<endl;
 	cout<<"零售税"<<fixed<<setprecision(2)<<b1*0.06<<endl;
@@ -149,6 +157,35 @@ void cash_managing(void)
 	cout<<"\n\n谢谢光临！";
 	books[i]-b;
 }
+void Bookinfo(BookData& book)
+{
+	cout<<"--------------------------------------------------------------------";
+	cout<<"\n\tnuaa图书管理系统\t\n\t\t书的资料\t\nISBN号："<<book.ISBN()<<"\n书 名："<<book.bookTitle()<<"\n库存量："<<book.qtyOnHand()<<"\n零售价："<<book.retail()<<"\n作者："<<book.bookauthor()<<endl;
+	cout<<"--------------------------------------------------------------------";
+}
+void lookUpBook(string a)
+{
+	int i=0;
+	while(isempty(i))
+	{
+		if (books[i].bookTitle() == a)
+			break;
+		i++;
+	}
+	if (!books[i].isexist)
+	{
+		cout<<"没这本书0.0看看别的"<<endl;
+		return;
+	}//剩下的都是在馆的（理论上
+	Bookinfo(books[i]);
+}
+
+void bookfile_managing(void)
+{
+
+
+}
+
 int main()
 {
  BookData books[200];//书库容器
@@ -157,9 +194,9 @@ int main()
     int qtyOnHand;
     double retail;
     string isbn;
-    ifstream basic_ifstream("bookfile");
+    ifstream basic_ifstream(R"(D:\code\clion\book regulating system\bookfile.txt)");
     int i=0;
-    if (basic_ifstream()==false)
+    if (!basic_ifstream)
     {
         cout<<"读取书库失败"<<endl;
         return 0;
@@ -172,8 +209,10 @@ int main()
         books[i].setISBN(isbn);
         books[i].setRetail(retail);
     	books[i].isexist=true;
+    	i++;
     }//读取书库
     basic_ifstream.close();//关闭文件输入流
-    void blockchoose();//进入模块选择
+	Bookinfo(books[0]);
+    blockchoose();//进入模块选择
     
 }
